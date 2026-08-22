6cb487a9f5beb7db76f3b283367cb97df276af60
Aura4G <aria4games@googlemail.com>
2026-08-22 01:03:51 +0100

    Added my drawn backgrounds to two existing items

--- src/main.cpp
+++ src/main.cpp
@@ -185,10 +185,10 @@ int main() {
     ResourceManager::loadTexture("lock", "media/images/lock.png");
 
     /* Backgrounds */
-    // Test
-    ResourceManager::loadTexture("test1", "media/images/backgrounds/test1.png");
-    ResourceManager::loadTexture("test2", "media/images/backgrounds/test2.png");
-    ResourceManager::loadTexture("test3", "media/images/backgrounds/test3.png");
+    // Outside
+    ResourceManager::loadTexture("outside1", "media/images/backgrounds/outside1.png");
+    ResourceManager::loadTexture("outside2", "media/images/backgrounds/outside2.png");
+    ResourceManager::loadTexture("outside3", "media/images/backgrounds/outside3.png");
 
     // Space
     ResourceManager::loadTexture("space1", "media/images/backgrounds/space1.png");
@@ -356,7 +356,7 @@ int main() {
     Button shopToggle(37.5f, 0.f, 470.f, 47.5f, REGULAR_BUTTON, "SHOP", "homeFont", ShapeType::Circle);
 
     Shop::addItem(Item(1, "Hint", "lock"));
-    Shop::addItem(Item(2, "Outdoor Theme", "settings", "test", ItemType::Background));
+    Shop::addItem(Item(2, "Outdoor Theme", "settings", "outside", ItemType::Background));
     Shop::addItem(Item(5, "Space Theme", "settings", "space", ItemType::Background));
 
     Shelf shopShelf(3, 2, sf::Vector2f(50.f,200.f), sf::Vector2f(150.f, 150.f), sf::Vector2f(25.f, 40.f));
